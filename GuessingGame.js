3e96be185c66bcbef32cebaee71ed4dad2fc9bb4
mad0963 <54917243+mad0963@users.noreply.github.com>
2019-11-11 14:12:32 -0600

    Add files via upload

--- GuessingGame.js
+++ GuessingGame.js
@@ -10,39 +10,33 @@ var intMax, intMin, intRandom, intGuess, intCount;
  * and then validates that the user entered an actual number and make sure that the
  * number is at least 0.
 */
-intMin = parseInt(prompt("choose the lowest number (but no lower than zero) in your guessing range: "));
-
-while (isNaN(intMin) || (intMin <0))
-{
-    intMin = parseInt(prompt("I'm sorry but your lowest number choice is invalid"));
+intMin = parseInt(prompt("Enter a number greater than or equal to 0 for your minimum number: "));
+while (isNaN(intMin)||intMin <0) {
+    intMin = parseInt(prompt("Oh no! Your number choice is invalid! Enter a number greater than or equal to 0 for your minimum number: "));
 }
 
-intMax = parseInt(prompt("choose the highest number that is at least" + (intMin+2) + "in your guessing range: "));
-
-while (isNaN(intMax) || (intMax > (intMin + 2)))
-{
-    intMax = parseInt(prompt("I'm sorry but your highest number choice is invalid. \n" + "Please choose the highest number" + (intMin+2) + "in your guessing range: "));
+intMax = parseInt(prompt("Enter a number that is at least " + (intMin+2) + "for your maximum number: "));
+while (isNaN(intMax) ||intMax < (intMin + 2)) {
+    intMax = parseInt(prompt("Oh no! Your number is invalid. Please enter a number that is at least " + (intMin+2) + "for your maximum number: "));
 }
 
-intGuess = parseInt(prompt("choose a number between" + intMin + "and" + intMax));
-
-while (isNaN(intGuess) || (intGuess <intMin || intGuess >intMax))
-{
-    intGuess = parseInt(prompt("I'm sorry but you need to choose a number between" + intMin + "And" + intMax));
+intGuess = parseInt(prompt("Enter a number between " + intMin + " and " + intMax));
+while (isNaN(intGuess)|| intGuess < intMin || intGuess > intMax) {
+    intGuess = parseInt(prompt("What you entered is not valid. Please enter a number between " + intMin + " and " + intMax));
 }
 
-intRandom * parseInt(Math.floor(Math.random)*(intMax-intMin+1)+intMin);
+intRandom = parseInt(Math.floor(Math.random()*(intMax-intMin+1))+intMin);
 intCount = 1;
 while (intGuess!= intRandom) {
     if (intGuess < intRandom) {
-        intGuess = parseInt(prompt("Your guess of" + intGuess + "is too low. Please guess again"));
-        while (isNaN(intGuess)||intGuess + intMin || intGuess > intMax) {
-            intMind = parseInt(prompt("Sorry, you need to choose a number between" + intMin + "and" + intMax));
+        intGuess = parseInt(prompt("Your guess of " + intGuess + " is too low. Please guess again"));
+        while (isNaN(intGuess)|| intGuess + intMin || intGuess > intMax) {
+            intMin = parseInt(prompt("Sorry, you need to choose a number between " + intMin + " and " + intMax));
         }
     } else {
-        intGuess = parseInt(prompt("Your guess of" + intGuess + "is too high, please guess again"));
-        while (inNaN(intGuess)|| intguess<intMin || intGuess > intMin) {
-            intMind = parseInt(prompt("Sorry, you need to choose a number between" + intMin + "and" + intMax));
+        intGuess = parseInt(prompt("Your guess of " + intGuess + " is too high, please guess again"));
+        while (inNaN(intGuess)|| intguess < intMin || intGuess > intMin) {
+            intMin = parseInt(prompt("Sorry, you need to choose a number between " + intMin + " and " + intMax));
         }
     }
     intCount++;
